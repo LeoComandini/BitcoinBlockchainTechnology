eb06c62602a3f48589b2ea8d9b2d39a5aa6d92ac
LeoComandini <leonardocomandini@hotmail.it>
2017-10-24 16:18:49 +0200

    complete the sign-to-contract commitment, still rough, without privkey

--- scripts/signToContractTest1.py
+++ scripts/signToContractTest1.py
@@ -9,8 +9,11 @@ from hashlib import sha256
 from os import urandom
 from secp256k1 import G, order, pointMultiply, pointAdd
 from FiniteFields import modInv
+from base58 import b58decode_check
 
 # build the sign-to-contract tx
+
+print("!!! This code doesn't provide a valid tx unless you know \n!!! and write the correct privKey")
 
 def hexStr2int(a):
     """ IN: hex str in little endian --> OUT: int
@@ -30,16 +33,28 @@ def signAndCommit(privKey,h,msg,k = None):
     if k == None:
         k = int.from_bytes(urandom(32), byteorder='big')
     R = pointMultiply(k, G)
-    e = hash(msg + ECpoint2compressedStr(R)) % order
+    assert msg != None
+    e = hash(msg + ECpoint2compressedStr(R)) % order # error, used a stupid hash function :(
     temp = pointMultiply(e, G)
     W = pointAdd(R, temp)
     h = int(h, 16)
-    z = ((h + R[0] * privKey) * modInv(k + e, order)) % order
+    z = ((h + W[0] * privKey) * modInv(k + e, order)) % order
     return (W[0], z), R
 
-# Data to redeem the tx output locked
+def txRawStr2txId(txRawStr):
+    txRawBytes = int(txRawStr, 16).to_bytes(len(txRawStr)//2, byteorder='big')
+    return sha256(sha256(txRawBytes).digest()).digest()[::-1].hex()
 
+# Data to redeem the tx output locked
+#  - outpointId (3.)
+#  - outpointIndex (4.)
+#  - amountPrevStr (9.) (to decide fee and new output amount)
+#
 # Data of the receiver
+#  - address in hex (11.)
+#
+# Commitment data:
+#  - msg (15.)
 
 # from https://bitcoin.stackexchange.com/questions/3374/how-to-redeem-a-basic-tx?noredirect=1&lq=1
 # follow the steps indicated to create the tx
@@ -50,8 +65,10 @@ version = int2hexStr(1,4) # 1
 numInput = int2hexStr(1,1) # 1
 # 3. tx id of the previous tx
 outpointId = "219d738419651dff4f9267a82efb5b8304d44f79d7d5787c88633dde27a18a2b"
+# rewrite in little endian (overkill)
+outpointId = int(outpointId, 16).to_bytes(len(outpointId)//2, byteorder='big')[::-1].hex()
 # 4. index of the previous tx
-outpointIndex = int2hexStr(1,4) # 1
+outpointIndex = int2hexStr(0,4) # 1st output
 # 6.
 outputScriptToRedeem = "76a914f156e1c0941d3e5b829e633d609fb52601b3350e88ac"
 # 5.
@@ -63,14 +80,15 @@ numOutput = int2hexStr(1,1) # 1
 # 9.
 amountPrevStr = "400d030000000000"
 amountPrev = hexStr2int(amountPrevStr)
-fee = 20000
+fee = 30000 #                                   FEE
 amount = amountPrev - fee
 assert amount > 0
-assert amount < amountPrev
+assert amount <= amountPrev
 amountStr = int2hexStr(amount, 8)
-# 11.
-address = "097072524438d003d23a2f23edb65aae1bb3e469" # TEMPPPPP
-outputScript = "76a914" + address + "88ac"
+# 11. receiving address
+receiveAddresWif = "1H83sqx7GtHLNfCGu4ptAUipnof3tXcS3"
+receiveAddresHex = b58decode_check(receiveAddresWif).hex()[2:]
+outputScript = "76a914" + receiveAddresHex + "88ac"
 # 10.
 lenOutputScript = hex(len(outputScript)//2)[2:]
 # 12. 
@@ -85,27 +103,31 @@ txForHashStr = version + numInput + outpointId + outpointIndex + \
 txForHashBytes = int(txForHashStr, 16).to_bytes(len(txForHashStr)//2, byteorder='big')
 h = dsha256(txForHashBytes)
 # 15. signing
-privKey = 1 # temp # corresponding to the pubKey of outputScriptToRedeem
+privKey = 1 # PRIVKEY (of the address in which the btc are locked)
+# another privKey was used! the one that could redeem the locked btc  
 msg = "test1 23oct2017 leonardo comandini"
 (x_W, z), R = signAndCommit(privKey,h,msg,k = None)
 # DER encoding
-DERfollows = '30'
+DERfollows = "30"
 x_WDER = ("00" if x_W > 2**(256-1)-1 else "") + hex(x_W)[2:].zfill(64)
 zDER   = ("00" if z   > 2**(256-1)-1 else "") + hex(  z)[2:].zfill(64)
 x_WDER = "02" + hex(len(x_WDER)//2)[2:].zfill(2) + x_WDER
 zDER   = "02" + hex(len(  zDER)//2)[2:].zfill(2) +   zDER
 lenSig = hex(len(x_WDER + zDER)//2)[2:].zfill(2) 
 hashCodeType = int2hexStr(1,1)
-pubKey = (1,1) #temp # pubkey of the script to redeem
-pubKeyStr = ECpoint2compressedStr(pubKey)
+#pubKey = (1,1) # PUBKEY (of the address in which the btc are locked)
+#pubKeyStr = ECpoint2compressedStr(pubKey)
+pubKeyStr = "039697e867ae7c317d60e3b5e1cc9d35cadb1178c422e567ec4aee82c6f46a06f3"
 lenPubKeyStr = hex(len(pubKeyStr)//2)[2:].zfill(2)
-DERsig = DERfollows + lenSig + x_WDER + zDER + hashCodeType + lenPubKeyStr + pubKeyStr
+
 # 16. concatenate
-lenSigPK = hex(len(DERsig)//2 + 1)[2:].zfill(2)
-DERsig = lenSigPK + DERsig
+DERsig = DERfollows + lenSig + x_WDER + zDER
+lenDERsig = hex(len(DERsig)//2 + 1)[2:].zfill(2) # +1 is for hashCodeType
+DERsig = lenDERsig + DERsig + hashCodeType + lenPubKeyStr + pubKeyStr
 # 17. We then replace the one-byte, varint length-field from step 5 with the  
 #     length of the data from step 16. The length is 106 bytes, or 0x6a bytes: 6a
-
+lenSigPK = hex(len(DERsig)//2)[2:].zfill(2)
+DERsig = lenSigPK + DERsig
 # 18. And we replace the temporary scriptSig from Step 6 with the data  
 #     structure constructed in step 16.
 
@@ -114,30 +136,50 @@ DERsig = lenSigPK + DERsig
 finalTx = version + numInput + outpointId + outpointIndex + \
           DERsig + sequenceNumber + numOutput + amountStr + \
           lenOutputScript + outputScript + nlocktime
+print("\n", finalTx, sep="")
 
-#01000000
-#01
-#219d738419651dff4f9267a82efb5b8304d44f79d7d5787c88633dde27a18a2b
-#01000000
-#6b
-#30
-#45
-#02
-#20
-#2ce6256f8f32d140ee020fcf40fae5edf3edacb7a50193ffab2992812592c9d4
-#02
-#21
-#00b28c7e7bf1f0562ba0e9f2d1f4b90deca38a977b82c2490c8894320682599d0b
-#01
-#21
-#03
-#0000000000000000000000000000000000000000000000000000000000000001
-#ffffffff
-#0120bf020000000000
-#1976a914097072524438d003d23a2f23edb65aae1bb3e46988ac
-#00000000
+nBytesFinalTx = len(finalTx)//2 # 193
+# maybe an assert?
+# source https://bitcoin.stackexchange.com/questions/1195/how-to-calculate-transaction-size-before-sending
+# see Edit2
+# N = numIn*148 + numOut*34 + 10 # case P2PKH # our case 1*148+1*34+10=192
+# assert nBytesFinalTx in (N-1,N,N+1)
 
+# check if the fees make sense with the market
+print("\nSatoshis per byte:",fee/nBytesFinalTx)
 
+# receipt
+print("\n*** Receipt:")
+print("     - txid :", txRawStr2txId(finalTx))
+print("     - ind  :", 0) # temp
+print("     - msg  :", msg)
+print("     - R    :", ECpoint2compressedStr(R))
 
-
-    
+### decompostition of the raw transaction
+#
+#01000000 ........................................................... version
+#01 ................................................................. numInput
+#219d738419651dff4f9267a82efb5b8304d44f79d7d5787c88633dde27a18a2b ... outpointId
+#00000000 ........................................................... outpointIndex
+# ................................................................... DERsig 
+#                  (it will be different for each rerunning, unless the eph privKey repeats)
+#6c .................................................................  - bytes redeem script
+#49 .................................................................  - bytes sig script
+#30 .................................................................  - DER follows
+#46 .................................................................  - bytes sig
+#02 .................................................................  - int follows
+#21 .................................................................  - bytes x_W
+#00f529fe1624b0851ec133ac2feeee5ec138d0879942231690ec6c1fe3b4cf491e .  - x_W
+#02 .................................................................  - int follows
+#21 .................................................................  - bytes z
+#00e600b9f8e1a0e5af5a87b346980d4c8501a3f72159aa7b8291b82fc16431f7b9 .  - z
+#01 .................................................................  - sighashcode
+#21 .................................................................  - bytes pubKey
+#03 .................................................................  - y_pubKey_parity
+#9697e867ae7c317d60e3b5e1cc9d35cadb1178c422e567ec4aee82c6f46a06f3 ...  - x_pubKey 
+#ffffffff ........................................................... sequenceNumber
+#01 ................................................................. numOutput
+#20bf020000000000 ................................................... amount
+#19 ................................................................. lenOutputScript
+#76a914030c8a43b75b3109875c94139b2b95734090737888ac ................. outputScript
+#00000000 ........................................................... nlocktime
